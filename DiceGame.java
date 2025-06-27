2ba27057500baf3e39618190141a6a90e7231ca1
Shishi-13 <s1320248@u-aizu.ac.jp>
2025-06-27 11:39:12 +0900

    Dice game: initial revision

--- DiceGame.java
+++ DiceGame.java
@@ -4,10 +4,12 @@ public class DiceGame{
     System.out.println("Dice rolling...");
 
     Random r = new Random();
+    int t=0;
 
     for(int i=1;i<3;i++){
       int dice=r.nextInt(6)+1;
-      System.out.print("Dice " + i + ": " + dice);
-      System.out.println();
-  }
+      System.out.println("Dice " + i + ": " + dice);
+      t+=dice;
+    }
+    System.out.println("total: " + t);
 }
