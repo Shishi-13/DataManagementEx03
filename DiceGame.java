8a420698b9616c44dba010bd377bc96fbe07d1e5
Shishi-13 <s1320248@u-aizu.ac.jp>
2025-06-27 11:46:38 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,6 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class DiceGame{
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("What is your name?");
+    String name=scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
     System.out.println("Dice rolling...");
 
     Random r = new Random();
@@ -12,4 +19,5 @@ public class DiceGame{
       t+=dice;
     }
     System.out.println("total: " + t);
+    scan.close();
 }
